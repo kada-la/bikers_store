7c28d420cd8f179188e83746983e7a2ccfbb4dd0
Kennedy Ada <K.Ada@landscapealliance.org>
2026-08-24 14:33:32 +0300

    feat(application): define product repository interface

--- src/SalesManagementSystem.Application/Interfaces/IGenericRepository.cs
+++ src/SalesManagementSystem.Application/Interfaces/IGenericRepository.cs
@@ -8,18 +8,11 @@ namespace SalesManagementSystem.Application.Interfaces;
 public interface IGenericRepository<T> where T : class
 {
     Task<T?> GetByIdAsync(int id);
-
     Task<IReadOnlyList<T>> GetAllAsync();
-
     Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate);
-
     Task AddAsync(T entity);
-
     void Update(T entity);
-
     void Remove(T entity);
-
     Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
-
     Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
 }
