669f022103adfcef4ac7b10a9e7ad326450d1d79
Kennedy Ada <K.Ada@landscapealliance.org>
2026-08-31 10:19:36 +0300

    feat(application): scaffold SaleService with unit of work dependency

---
+++ src/SalesManagementSystem.Application/Services/SaleService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesManagementSystem.Application.Interfaces;
+using SalesManagementSystem.Application.DTOs;
+using SalesManagementSystem.Domain.Common;
+using SalesManagementSystem.Domain.Entities;
+
+namespace SalesManagementSystem.Application.Services;
+
+public class SaleService : ISaleService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SaleService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+}
