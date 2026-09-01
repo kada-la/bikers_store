6fa3d20e505512ada6a715ab81070cf1469bc04a
Kennedy Ada <K.Ada@landscapealliance.org>
2026-09-01 10:27:06 +0300

    fix(infrastructure): add missing DTOs import in ReportRepository

--- src/SalesManagementSystem.Infrastructure/Repositories/ReportRepository.cs
+++ src/SalesManagementSystem.Infrastructure/Repositories/ReportRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesManagementSystem.Application.Interfaces;
+using SalesManagementSystem.Application.DTOs;
 using SalesManagementSystem.Domain.Entities;
 using SalesManagementSystem.Infrastructure.Persistence;
 
